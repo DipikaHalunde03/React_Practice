17c1cc1e1804406ae05a049268371ec5b905e7d0
DipikaHalunde03 <dh071844@gmail.com>
2024-07-09 19:09:16 +0530

    first react app created

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Chai from  './simple1.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <Chai/>
+  </React.StrictMode>,
+)
---
+++ vite-project/src/simple1.jsx
@@ -0,0 +1,11 @@
+function Chai()
+{
+    return (
+        <h1>
+         your chai is ready 
+        </h1>
+    
+       
+      );
+}
+export default Chai
