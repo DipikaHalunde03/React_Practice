15b0c3e2ee17bb161d9135f7524124b84e2bddda
DipikaHalunde03 <dh071844@gmail.com>
2024-07-10 21:44:26 +0530

    structure of react project

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import Chai from  './simple1.jsx'
 
-
+import App from "./App";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Chai/>
+  <App/>
   </React.StrictMode>,
 )
--- vite-project/src/simple1.jsx
+++ vite-project/src/simple1.jsx
@@ -1,10 +1,21 @@
+import App from "./App";
+
+
 function Chai()
 {
     return (
-        <h1>
+      <>
+
+<App/>
+
+       <h1>
          your chai is ready 
         </h1>
-    
+      
+        <p>  Hello , I am learning react !!  </p>
+
+         
+      </>
        
       );
 }
